421facf49eee73ecf640346d97b9960cd6bfb316
Halil Kesmez <halilkesmez@gmail.com>
2020-06-08 23:08:46 +0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> kutup ayilari yalniz
+          Edit <code>src/App.js</code> DEGISTIRDIM BIR SEYLER
         </p>
         <a
           className="App-link"
